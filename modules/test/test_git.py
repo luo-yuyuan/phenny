3731b1833bb2191a8966e510c561e0b3456e07df
luo-yuyuan <yuyuanluo.fhc@gmail.com>
2018-11-01 23:23:44 -0400

    remove token

--- modules/test/test_git.py
+++ modules/test/test_git.py
@@ -28,11 +28,4 @@ class TestGit(unittest.TestCase):
         git.post_issue(self.phenny, self.input)
         self.phenny.say.assert_called_once_with(
             "Please try again. Usage: .issue <org>/<repo> <handle> <password> <issue title>")
-
-    @catch_timeout
-    def test_issue_validPassword(self):
-        self.input.group = lambda x: [None, 'luo-yuyuan/phenny luo-yuyuan 99bcc58e1642c398ac1ee420696c1356d0386786 Issue Title'][x]
-        git.post_issue(self.phenny, self.input)
-        self.phenny.say.assert_called_once_with(
-            "Issue created. You can add a description at https://github.com/luo-yuyuan/phenny/issues")
         
